96e2ab6ae77b99350d9637b391628c558ebf09d1
manali0501 <manali.khedekar501@gmail.com>
2024-08-17 17:36:28 +0530

    How add value to products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,15 @@ import { Testimonials } from "@/sections/Testimonials";
 import { CallToAction } from "@/sections/CallToAction";
 import { Footer } from "@/sections/Footer";
 import IndustriesWeServe from "@/sections/IndustriesWeServe";
+import ValuetoProduct from "@/sections/ValuetoProduct";
+
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <ValuetoProduct/>
       <IndustriesWeServe/>
       <LogoTicker />
       <ProductShowcase />
---
+++ src/sections/ValuetoProduct.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const ValuetoProduct = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-6">
+      <h1 className="text-2xl font-bold mb-6">How we Add value to our Products</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {/* First row */}
+        <div className="bg-white text-gray-700 border border-gray-300 p-6 rounded-xl flex items-center justify-center h-32">
+          Hardware
+        </div>
+        <div className="bg-orange-400 text-white p-6 rounded-xl flex items-center justify-center h-32">
+          Selection Of right Products
+        </div>
+        <div className="bg-orange-400 text-white p-6 rounded-xl flex items-center justify-center h-32">
+          Engineering Products
+        </div>
+        
+        {/* Second row */}
+        <div className="bg-orange-500 text-white p-6 rounded-xl flex items-center justify-center h-48 col-span-2">
+          Instrumentation & System Integration
+        </div>
+        <div className="bg-red-400 text-white p-6 rounded-xl flex items-center justify-center h-38">
+          Energy Monitoring Systems
+        </div>
+        <div className="bg-red-400 text-white p-6 rounded-xl flex items-center justify-center h-34">
+          Safety Sensor Solutions
+        </div>
+        
+        {/* Third row */}
+        <div className="bg-orange-400 text-white p-6 rounded-xl flex items-center justify-center h-48 col-span-2">
+        Corrective & Preventive Maintenance
+        </div>
+        <div className="bg-white text-gray-700 border border-gray-300 p-6 rounded-xl flex items-center justify-center h-32 col-span-3">
+        Custom Programming for PLC, HMI, SCADA, VFD, Servo
+</div>
+
+      </div>
+    </div>
+  );
+};
+
+export default ValuetoProduct;

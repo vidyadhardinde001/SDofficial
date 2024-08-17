a19ba28cc63d91217e90a0a1f2cc3e609f40c102
vidyadhardinde <135449569+vidyadhardinde@users.noreply.github.com>
2024-08-17 10:40:19 +0530

    header final

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
 import ArrowRight from "@/assets/arrow-right.svg";
-import Logo from "@/assets/logosaas.png";
+import logo from "@/assets/logo.png";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu.svg";
 
@@ -18,28 +18,33 @@ export const Header = () => {
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            <Image src={Logo} alt="Saas Logo" height={40} width={40} />
+            <Image src={logo} alt="Saas Logo" height={30} width={130} />
             <div className="md:hidden ml-auto">
               <MenuIcon className="h-5 w-5" />
             </div>
-            <nav className="hidden md:flex gap-14 text-black/60 justify-center items-center bg-white px-4 py-2 rounded-full sm:w-[320px] md:w-[590px] lg:w-[700px] xl:w-[1200px] file: max-w-screen-md mx-auto">
-              <a href="#" className="hover:text-black">
+            <nav className="hidden md:flex lg:gap-14 md:gap-8 text-black/60 justify-center items-center bg-white px-4 py-2 rounded-full sm:w-[320px] md:w-[500px] lg:w-[700px] xl:w-[1200px] max-w-screen-md mx-auto">
+              <a href="#" className="relative group hover:text-black">
                 Home
+                <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#fb845d] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
               </a>
-              <a href="#" className="hover:text-black">
+              <a href="#" className="relative group hover:text-black">
                 Projects
+                <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#fb845d] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
               </a>
-              <a href="#" className="hover:text-black">
+              <a href="#" className="relative group hover:text-black">
                 Gallery
+                <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#fb845d] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
               </a>
-              <a href="#" className="hover:text-black">
+              <a href="#" className="relative group hover:text-black">
                 Contact
+                <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#fb845d] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
               </a>
-              <a href="#" className="hover:text-black">
+              <a href="#" className="relative group hover:text-black">
                 About Us
+                <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#fb845d] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
               </a>
             </nav>
-            <button className="bg-[#fb845d] text-white px-4 py-2 rounded-full font-medium inline-flex align-items justify-center tracking-tight ml-auto hidden md:block">
+            <button className="bg-[#fb845d] hover:bg-black text-white px-4 py-2 rounded-full font-medium inline-flex align-items justify-center tracking-tight ml-auto hidden md:block">
               Call Us
             </button>
           </div>

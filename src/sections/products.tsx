6aac4aa41752b8445ffee3eac55fed0408b7d89e
NikhilS1622 <Nikhil.sutar9@gmail.com>
2025-03-05 21:57:25 +0530

    Update products.tsx

--- src/sections/products.tsx
+++ src/sections/products.tsx
@@ -12,13 +12,18 @@ interface Product {
 
 const hardcodedData: Product[] = [
   {
-    name: "Lubi Catalog",
-    image: "https://drive.google.com/uc?export=view&id=1BVqrRRB4Plnn7OOtJ1uWlo0GbeFapR84",
+    name: "HMI",
+    image: "https://drive.google.com/uc?export=view&id=1yUosAAeAGk3OIlATMktF0L_uiHbRyj0F",
+    pdf: "https://drive.google.com/file/d/1ftLX6aqz0USMwO4uqQhPn2k_qO_ETT5i/view?usp=sharing",  // Open PDF in browser
+  },
+  {
+    name: "PLC",
+    image: "https://drive.google.com/uc?export=view&id=1B_luix_ZiNqFvloy4RkbEJKbnKahfAmq",
     pdf: "https://drive.google.com/file/d/1ftLX6aqz0USMwO4uqQhPn2k_qO_ETT5i/view?usp=sharing",  // Open PDF in browser
   },
   {
     name: "SMPS",
-    image: "https://drive.google.com/uc?export=view&id=1HN1QWq4McL3yEkattEwwryY3hI30eAVJ",
+    image: "https://drive.google.com/uc?export=view&id=1eRy-FPqDGC_aom09Co2p-M2ORrBL4Ki3",
     pdf: "https://drive.google.com/file/d/1ftLX6aqz0USMwO4uqQhPn2k_qO_ETT5i/view?usp=sharing",  // Open PDF in browser
   },
   {
